3c4e0a2f7aba6b77236641ce8361b940743344e1
Juan Miguel Núñez Galán <miguel@Juans-MacBook-Pro.local>
2017-01-31 18:22:23 -0500

    uploaded draw

--- Week 4/sketch.js
+++ Week 4/sketch.js
@@ -1,6 +1,382 @@
 function setup() {
-	createCanvas(640, 360);
+	createCanvas(1080, 480);
+	// Your sketch code
 }
+
 function draw(){
-	rect(100,200,75,150)
+	
+	//stripe1
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1,1,1079,37);
+	//stripe2
+	fill(255,255,255)
+	rect(1,37, 1079,37)
+	//stripe3
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1,74, 1079, 37)
+	//stripe4
+	fill(255,255,255)
+	rect(1,111,1079, 37)
+	//stripe5
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1, 148, 1079, 37)
+	//stripe6
+	fill(255,255,255)
+	rect(1, 185, 1079, 37)
+	//stripe7
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1,222,1079,37)
+	//stripe8
+	fill(255,255,255)
+	rect(1,259,1079,37)
+	//stripe9
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1,296,1079,37)
+	//stripe10
+	fill(255,255,255)
+	rect(1,333, 1079, 37)
+	//stripe11
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1,370,1079,37)
+	//stripe12
+	fill(255,255,255)
+	rect(1, 403, 1079, 37)
+	//stripe13
+	fill(224,22,43)
+	stroke(224,22,43)
+	rect(1, 440, 1079, 37)
+	//blue
+	fill(0,82,165)
+	stroke(0,82,165)
+	rect(1, 1, 400, 258)
+	//row1
+	//star1
+	fill(255,255,255)
+	ellipse(25,25,25,25)
+	//star2
+	fill(255,255,255)
+	ellipse(95,25,25,25)
+	//star3
+	fill(255,255,255)
+	ellipse(165,25,25,25)
+	//star4
+	fill(255,255,255)
+	ellipse(235,25,25,25)
+	//star5
+	fill(255,255,255)
+	ellipse(305,25,25,25)
+	//star6
+	fill(255,255,255)
+	ellipse(375,25,25,25)
+	//row2
+	//star7
+	fill(255,255,255)
+	ellipse(60,50,25,25)
+	//star8
+	fill(255,255,255)
+	ellipse(130,50,25,25)
+	//star9
+	fill(255,255,255)
+	ellipse(200,50,25,25)
+	//star10
+	fill(255,255,255)
+	ellipse(270,50,25,25)
+	//star11
+	fill(255,255,255)
+	ellipse(340,50,25,25)
+	//row3
+	//star12
+	fill(255,255,255)
+	ellipse(25,75,25,25)
+	//star13
+	fill(255,255,255)
+	ellipse(95,75,25,25)
+	//star14
+	fill(255,255,255)
+	ellipse(165,75,25,25)
+	//star15
+	fill(255,255,255)
+	ellipse(235,75,25,25)
+	//star16
+	fill(255,255,255)
+	ellipse(305,75,25,25)
+	//star17
+	fill(255,255,255)
+	ellipse(375,75,25,25)
+	//row4
+	//star18
+	fill(255,255,255)
+	ellipse(60,100,25,25)
+	//star19
+	fill(255,255,255)
+	ellipse(130,100,25,25)
+	//star20
+	fill(255,255,255)
+	ellipse(200,100,25,25)
+	//star21
+	fill(255,255,255)
+	ellipse(270,100,25,25)
+	//star22
+	fill(255,255,255)
+	ellipse(340,100,25,25)
+	//row5
+	//star23
+	fill(255,255,255)
+	ellipse(25,125,25,25)
+	//star24
+	fill(255,255,255)
+	ellipse(95,125,25,25)
+	//star25
+	fill(255,255,255)
+	ellipse(165,125,25,25)
+	//star26
+	fill(255,255,255)
+	ellipse(235,125,25,25)
+	//star27
+	fill(255,255,255)
+	ellipse(305,125,25,25)
+	//star28
+	fill(255,255,255)
+	ellipse(375,125,25,25)
+	//row6
+	//star29
+	fill(255,255,255)
+	ellipse(60,150,25,25)
+	//star30
+	fill(255,255,255)
+	ellipse(130,150,25,25)
+	//star31
+	fill(255,255,255)
+	ellipse(200,150,25,25)
+	//star32
+	fill(255,255,255)
+	ellipse(270,150,25,25)
+	//star33
+	fill(255,255,255)
+	ellipse(340,150,25,25)
+	//row7
+	//star34
+	fill(255,255,255)
+	ellipse(25,175,25,25)
+	//star35
+	fill(255,255,255)
+	ellipse(95,175,25,25)
+	//star36
+	fill(255,255,255)
+	ellipse(165,175,25,25)
+	//star37
+	fill(255,255,255)
+	ellipse(235,175,25,25)
+	//star38
+	fill(255,255,255)
+	ellipse(305,175,25,25)
+	//star39
+	fill(255,255,255)
+	ellipse(375,175,25,25)
+	//row8
+	//star40
+	fill(255,255,255)
+	ellipse(60,200,25,25)
+	//star41
+	fill(255,255,255)
+	ellipse(130,200,25,25)
+	//star42
+	fill(255,255,255)
+	ellipse(200,200,25,25)
+	//star43
+	fill(255,255,255)
+	ellipse(270,200,25,25)
+	//star44
+	fill(255,255,255)
+	ellipse(340,200,25,25)
+	//row9
+	//star45
+	fill(255,255,255)
+	ellipse(25,225,25,25)
+	//star46
+	fill(255,255,255)
+	ellipse(95,225,25,25)
+	//star47
+	fill(255,255,255)
+	ellipse(165,225,25,25)
+	//star48
+	fill(255,255,255)
+	ellipse(235,225,25,25)
+	//star49
+	fill(255,255,255)
+	ellipse(305,225,25,25)
+	//star50
+	fill(255,255,255)
+	ellipse(375,225,25,25)
+	//trump
+	//row1
+	fill(255,199,18)
+	stroke(255,199,18)
+	rect(540, 40, 25,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(565,40,25,25)
+	fill(255,199,18)
+	stroke(255,199,18)
+	rect(590, 40, 25,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(615,40,25,25)
+	fill(255,199,18)
+	stroke(255,199,18)
+	rect(640, 40, 25,25)
+	//row2
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(534,65,15,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(540, 65, 25,25)
+	fill(255,199,18)
+	stroke(255,199,18)
+	rect(565,65,25,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(590, 65, 25,25)
+	fill(255,199,18)
+	stroke(255,199,18)
+	rect(615,65,25,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(640, 65, 25,25)
+	fill(221,166,1)
+	stroke(221,166,1)
+	rect(661,65,15,25)
+	//row3
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(540,90,25,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(565,90,25,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(590,90,25,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(615,90,25,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(640,90,25,25)
+	//row4
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(540,115,15,25)
+	fill(244,183,9)
+	stroke(244,183,9)
+	rect(555,115,35,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(590, 115,25,25)
+	fill(244,183,9)
+	stroke(244,183,9)
+	rect(615,115,35,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(650,115,15,25)
+	//row5
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(540,140,15,25)
+	fill(255,255,255)
+	stroke(255,255,255)
+	rect(555,140,20,25)
+	fill(0,0,0)
+	stroke(0,0,0)
+	rect(575,140,15,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(590, 140,25,25)
+	fill(0,0,0)
+	stroke(0,0,0)
+	rect(615,140,15,25)
+	fill(255,255,255)
+	stroke(255,255,255)
+	rect(630,140,20,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(650,140,15,25)
+	//row6
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(540, 165, 15,15)
+	fill(250,156,46)
+	stroke(250,156,46)
+	rect(555,165,35,15)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(590,165,25,15)
+	fill(250,156,46)
+	stroke(250,156,46)
+	rect(615,165,35,15)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(650,165,15,15)
+	//row7
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(540,180,15,25)
+	fill(255,255,255)
+	stroke(255,255,255)
+	rect(555,180,95,25)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(650,180,15,25)
+	//row8
+	fill(0,0,50)
+	stroke(0,0,50)
+	rect(490,205,80,55)
+	fill(245,63,162)
+	stroke(245,63,162)
+	rect(570,205,15,15)
+	fill(255,255,255)
+	stroke(255,255,255)
+	rect(570, 220, 60, 40)
+	fill(221,143,47)
+	stroke(221,143,47)
+	rect(585,205,30,15)
+	fill(245,63,162)
+	stroke(245,63,162)
+	rect(615,205,15,15)
+	fill(0,0,50)
+	stroke(0,0,50)
+	rect(630,205,80,55)
+	fill(240,62,156)
+	stroke(240,62,156)
+	rect(585, 220, 30,30)
+
+
+
+
+
+
+
+	
+//function star(x,y, radius1, radius2, npoints){
+//	var angle= TWO_PI/npoints;
+//	var halfangle= angle/2.0;
+//	beginShape();
+//	for (var a = 0; a<TWO_PI; a+=angle)
+//		var sx=x+cos(a)*radius2;
+//		var sy=y+sin(a)*radius2;
+//		vertex(sx,sy);
+//		sx=x+cos(a+halfangle)*radius1;
+//		sy = y + sin(a+halfAngle) * radius1;
+//    vertex(sx, sy);
+//  }
+//  endShape(CLOSE);
+//	}
+	// Your sketch code
 }
+
