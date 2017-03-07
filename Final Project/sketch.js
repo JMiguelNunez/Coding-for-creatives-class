9a9ef724f20e3e5e6f086b252cf9888bfc3d109b
Juan Miguel Núñez Galán <miguel@Juans-MacBook-Pro.local>
2017-03-07 00:13:27 -0500

    uploaded final project

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,9 +1,3 @@
-var ballSpeedX = 5;
-var ballSpeedY = 3;
-var ballR = 10;
-var ballX = 200;
-var ballY = 200;
-
 var brick1=true;
 var brick2=true;
 var brick3=true;
@@ -12,129 +6,224 @@ var brick5=true;
 var brick6=true;
 var brick7=true;
 var brick8=true;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+var brick9=true;
+var brick10=true;
+var brick11=true;
+var brick12=true; 
+var paddleWidth = 100;
+var paddleX;
+var paddleY;
+var distance;
+
+// the speed of the ball when it first leaves the paddle
+var initialSpeedX = 5;
+var initialSpeedY = -10;
+
+// the current speed of the ball
+var ballSpeedX = initialSpeedX;
+var ballSpeedY = initialSpeedY;
+
+// the current location of the ball
+var ballX = 0 ;
+var ballY = 0;
+
+var img;
+
+var lives = 3;
+
+// true if ball is moving, false if ball is attached to paddle
+var ballMoving = false;
+function preload(){
+    img=loadImage('Media/heart.png')
 }
+function setup() {
+  createCanvas(600, windowHeight);
+  paddleY = windowHeight-50;
+  image(img, width/2, height/2, 50, 50);
+}
+
 var draw = function() {
+
     background(0);
-    fill(255,255,0);
-    ellipse(ballX,ballY, 20, 20);
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-    
-    if(ballX+ballR > windowWidth-10 || ballX-ballR< 0){
-        ballSpeedX *= -1;
+    for (var i = 0; i < lives; i++) {
+        image(img, 0 + i * 50, 0, 30, 30);
     }
     
-    if(ballY+ballR > windowHeight-10 || ballY-ballR < 0){
-        ballSpeedY *= -1;
+   
+    // draw the paddle
+    fill(240, 126, 65);
+    paddleX = mouseX - paddleWidth/2;
+    rect(paddleX, paddleY, paddleWidth, 20);
+    
+    // move the ball
+    if (ballMoving) {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+    }
+    else {
+        ballX = mouseX;
+        ballY = windowHeight-60;
     }
     
-    //check if hit a brick and drawing bricks
+    // draw the ball
+    fill(255, 234, 0);
+    ellipse(ballX, ballY, 20, 20);
+    
+    // ball hits top
+    if (ballY <= 50) {
+        ballSpeedY = -ballSpeedY;
+    }
+    // ball hits left
+    if (ballX <= 10) {
+        ballSpeedX = -ballSpeedX;
+    }
+    // ball hits right
+    if (ballX >= 600) {
+        ballSpeedX = -ballSpeedX;
+    }
+    // ball hits paddel
+    if (ballY >= windowHeight-50 && ballX >= mouseX - paddleWidth/2 && ballX <= mouseX + paddleWidth/2) {
+        ballSpeedY = -ballSpeedY;
+    }
+    //ball respawn
+    if (ballY >= windowHeight-10) {
+        ballMoving = false;
+        lives=i-1;
+    }
+    if (ballY>=windowHeight-10 && lives=0){
+        textSize(100);
+        text("GAME OVER!", windowWidth/2, windowHeight/2)
+    }
+    //ball hits the brick 
     if(brick1===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 0 && ballX-ballR <= 100){
-                brick2=false;
+        if(ballY< 69){
+            if(ballX >= 0 && ballX<= 100){
+                brick1=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick1===true){
             fill(0,255,0);
-            rect(0,0,100, 40);
+            rect(0,30,100, 40);
         }
     }
-
-    
-    if(brick2===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 100 && ballX-ballR <= 200){
+ if(brick2===true){
+        if(ballY< 69){
+            if(ballX>= 100 && ballX<= 200){
                 brick2=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick2===true){
             fill(0,255,0);
-            rect(100,0,100, 40);
+            rect(100,30,100, 40);
         }
     }
-
     if(brick3===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 200 && ballX-ballR <= 300){
+        if(ballY< 69){
+            if(ballX >= 200 && ballX <= 300){
                 brick3=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick3===true){
             fill(0,255,0);
-            rect(200,0,100, 40);
+            rect(200,30,100, 40);
         }
     }
-    
     if(brick4===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 300 && ballX-ballR <= 400){
+        if(ballY< 69){
+            if(ballX >= 300 && ballX <= 400){
                 brick4=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick4===true){
             fill(0,255,0);
-            rect(300,0,100, 40);
+            rect(300,30,100, 40);
         }
     }
     if(brick5===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 400 && ballX-ballR <= 500){
+        if(ballY< 69){
+            if(ballX >= 400 && ballX <= 500){
                 brick5=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick5===true){
             fill(0,255,0);
-            rect(400,0,100, 40);
+            rect(400,30,100, 40);
         }
-    }if(brick6===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 500 && ballX-ballR <= 600){
+    }
+    if(brick6===true){
+        if(ballY< 69){
+            if(ballX >= 500 && ballX <= 600){
                 brick6=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick6===true){
             fill(0,255,0);
-            rect(500,0,100, 40);
+            rect(500,30,100, 40);
         }
-    }if(brick7===true){
-        if(ballY-ballR < 40){
-            if(ballX+ballR >= 600 && ballX-ballR <= 700){
+    }
+    if(brick7===true){
+        if(ballY< 109){
+            if(ballX >= 0 && ballX <= 100){
                 brick7=false;
                 ballSpeedY *= -1;
             }
         } 
         if(brick7===true){
-            fill(0,255,0);
-            rect(600,0,100, 40);
+            fill(0,0,255);
+            rect(0,70,100, 40);
         }
-    }if(brick8===true){
-        if(ballY-ballR<40);{
-            if(ballY+ballR>=40 && ballY-ballR<=80)
-             if(ballX+ballR>=0 && ballX-ballR<=40){
+    }
+    if(brick8===true){
+        if(ballY< 109){
+            if(ballX >= 100 && ballX <= 200){
                 brick8=false;
                 ballSpeedY *= -1;
             }
-        }
-    }
-        if(brick8==true){
+        } 
+        if(brick8===true){
             fill(0,0,255);
-            rect(0,40,100,40)
+            rect(100,70,100, 40);
         }
-    
-    //draw paddle
-    fill(255,0,0);
-    rect(mouseX-50, windowHeight-50, 100,20);
-    if(ballX+ballR > mouseX-100 && ballX-ballR < mouseX + 100 && ballY+ballR > 370){
-        ballSpeedY *= -1;
     }
-    
+    if(brick9===true){
+        if(ballY< 109){
+            if(ballX >= 200 && ballX <= 300){
+                brick9=false;
+                ballSpeedY *= -1;
+            }
+        } 
+        if(brick9===true){
+            fill(0,0,255);
+            rect(200,70,100, 40);
+        }
+    }
+    if(brick10===true){
+        if(ballY< 109){
+            if(ballX >= 300 && ballX <= 400){
+                brick10=false;
+                ballSpeedY *= -1;
+            }
+        } 
+        if(brick10===true){
+            fill(0,0,255);
+            rect(300,70,100, 40);
+        }
+    }
+
 }
+
+var mouseClicked = function() {
+    if (!ballMoving) {
+        // reset the ball speed
+        ballSpeedX = initialSpeedX;
+        ballSpeedY = initialSpeedY;
+        
+        ballMoving = true;
+    }
+};
